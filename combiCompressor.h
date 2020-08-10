2fd8aabef58062a29dd3fc006732e9f400bd21ab
BeatEngine <32369318+BeatEngine@users.noreply.github.com>
2020-08-10 10:54:28 +0200

    Lempel-Ziv-Huffmann-compression

--- combiCompressor.h
+++ combiCompressor.h
@@ -24,5 +24,27 @@ class CompressorLZ77Huffman
         return result;
     }
 
+    static unsigned char* compress2(unsigned char* data, size_t size, size_t* returnSizeSymbolHead, size_t* returnSize)
+    {
+        size_t compressedSize;
+        Huffmann::CodeTable table = Huffmann::compress((char*)data, size, false);
+        char* comp = table.getFullData(returnSizeSymbolHead);
+        size_t retSize = table.resultSize + *returnSizeSymbolHead;
+        unsigned char* ret = LZWcompression::compress(comp, retSize, returnSize);
+        free(comp);
+        return ret;
+    }
+
+    static unsigned char* decompress2(unsigned char* data, size_t size, size_t symbolHeadSize, size_t* returnSize)
+    {
+        size_t sizeHuffmanDecompressed = 0;
+        char* decomp = LZWcompression::decompress(data, size, &sizeHuffmanDecompressed);
+        Huffmann::CodeTable result((char*)decomp, symbolHeadSize);
+        result.result = (char*)(decomp+symbolHeadSize);
+        result.resultSize = sizeHuffmanDecompressed - symbolHeadSize;
+        unsigned char* res = (unsigned char*)result.decompress((int*)returnSize);
+        free(decomp);
+        return res;
+    }
 };
 
